1e65fcf483166bcc6175e2cb51e33a873b956f43
Małgorzata Sieńska <235965@edu.p.lodz.pl>
2022-05-08 21:13:44 +0200

    10. Hermetyzacja nieprywatnych atrybutów

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,38 +6,57 @@ public class DefaultCountingOutRhymer {
     private static final int MAX_OF_NUMBERS = 12;
     private static final int ERROR = -1;
     private static final int NONE_NUMBERS = -1;
-    private final int[] NUMBERS = new int[MAX_OF_NUMBERS];
+    private final int[] NUMBERS = new int[getMaxOfNumbers()];
+
+    public static int getTOTAL() {
+        return TOTAL;
+    }
+
+    public static int getMaxOfNumbers() {
+        return MAX_OF_NUMBERS;
+    }
+
+    public static int getERROR() {
+        return ERROR;
+    }
+
+    public static int getNoneNumbers() {
+        return NONE_NUMBERS;
+    }
 
     public int getTotal() {
         return total;
     }
 
-    public int total = NONE_NUMBERS;
+    public int total = getNoneNumbers();
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            getNUMBERS()[++total] = in;
     }
 
     public boolean callCheck() {
-        return total == NONE_NUMBERS;
+        return total == getNoneNumbers();
     }
 
     public boolean isFull() {
-        return total == TOTAL;
+        return total == getTOTAL();
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return ERROR;
+            return getERROR();
 
-        return NUMBERS[total];
+        return getNUMBERS()[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return ERROR;
-        return NUMBERS[total--];
+            return getERROR();
+        return getNUMBERS()[total--];
     }
 
+    public int[] getNUMBERS() {
+        return NUMBERS;
+    }
 }
