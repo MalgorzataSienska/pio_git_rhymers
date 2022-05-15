727293d3db71360e40d085ed07ec96f3b015bef0
Małgorzata Sieńska <235965@edu.p.lodz.pl>
2022-05-15 21:56:42 +0200

    14. Walidacja

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
-
+	//test passed
 	@Test
 	public void testCountIn() {
 		DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
